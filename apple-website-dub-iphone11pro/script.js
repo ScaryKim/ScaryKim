ccc3e1d598046aa0e93dcc14af69477ca9a5c72c
ScaryKim <91406817+ScaryKim@users.noreply.github.com>
2022-04-09 14:23:35 +0800

    Add files via upload

---
+++ apple-website-dub-iphone11pro/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const selectElement = (element) => document.querySelector(element);
+
+selectElement(".mobile-menu").addEventListener("click", () => {
+  selectElement("header").classList.toggle("active");
+});
